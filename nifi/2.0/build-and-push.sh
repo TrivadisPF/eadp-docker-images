faaf142e0f94022dbe4025196704cdecf33ad4b9
Guido Schmutz <schmutz68@gmail.com>
2025-02-16 20:13:01 +0100

    add nifi 2.1.0 and 2.2.0

--- nifi/2.0/build-and-push.sh
+++ nifi/2.0/build-and-push.sh
@@ -5,6 +5,6 @@ cd "$(dirname "$0")"
 
 docker build -t eadp/nifi:2.0.0 .
 
-docker tag eadp/nifi:2.2.0 $DOCKER_REGISTRY/eadp/nifi:2.0.0
+docker tag eadp/nifi:2.0.0 $DOCKER_REGISTRY/eadp/nifi:2.0.0
 
 docker push $DOCKER_REGISTRY/eadp/nifi:2.0.0
---
+++ nifi/2.1/build-and-push.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Change to the directory where the script is located
+cd "$(dirname "$0")"
+
+docker build -t eadp/nifi:2.1.0 .
+
+docker tag eadp/nifi:2.1.0 $DOCKER_REGISTRY/eadp/nifi:2.1.0
+
+docker push $DOCKER_REGISTRY/eadp/nifi:2.1.0
